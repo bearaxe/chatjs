741ed375fe7cedf3ea5ccb394a638bb033accd2d
bearaxe <20572230+bearaxe@users.noreply.github.com>
2021-01-16 16:38:25 -0500

    get backend set up with nodemon

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A HelloWorld for Socket.io based off https://itnext.io/build-a-group-chat-app-in-30-lines-using-node-js-15bfe7a2417b
 
+File structure and server interactions theived from https://medium.com/bb-tutorials-and-thoughts/how-to-develop-and-build-vue-js-app-with-nodejs-bd86feec1a20
+
 ## Project setup
 ```
 npm install
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = 3080;
+const frontendDir = '../frontend/dist';
 
+app.use(express.static(path.join(__dirname, frontendDir)));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/../public/index.html'));
+  res.sendFile(path.join(__dirname + frontendDir + '/index.html'));
 });
 
-const server = http.listen(8080, function() {
-  console.log('listening on *:8080');
+const server = http.listen(port, function() {
+  console.log(`listening on *:${port}`);
 });
