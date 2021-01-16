cd3bfee9bc4f41919152b9829eeb3cfd8635829e
bearaxe <20572230+bearaxe@users.noreply.github.com>
2021-01-16 15:30:16 -0500

    copy over interface from tutorial

---
+++ src/components/ChatBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <form action="/" method="POST" id="chatForm">
+    <input type="text" id="txt" autocomplete="off" placeholder="type your message here..."/>
+    <button>Send</button>
+  </form>
+</template>
+
+<style scoped>
+form { 
+  background: #fff; 
+  padding: 3px;
+  position: fixed; 
+  bottom: 0; 
+  width: 100%; 
+  border-color: #000;
+  border-top-style: solid; 
+  border-top-width: 1px;
+}
+form input { 
+  border-style: solid; 
+  border-width: 1px; 
+  padding: 10px; 
+  width: 85%; 
+  margin-right: .5%; 
+}
+form button { 
+  width: 9%; 
+  background: rgb(130, 224, 255); 
+  border: none; 
+  padding: 10px; 
+  margin-left: 2%; 
+}
+</style>
---
+++ src/components/ChatMessages.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul id="messages"></ul>
+</template>
+
+<style scoped>
+#messages { 
+  list-style-type: none; 
+  margin: 0;
+  padding: 0; 
+}
+#messages li { 
+  padding: 5px 10px; 
+}
+#messages li:nth-child(odd) { 
+  background: #eee; 
+}
+</style>
